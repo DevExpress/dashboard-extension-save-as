e25bd30616d93604e948a6251140acbd75cb06a4
rudnev.dmitry <dmitry.rudnev@devexpress.com>
2017-05-18 16:39:38 +0300

    initial commit

---
+++ save-as-extension.js
@@ -0,0 +1,28 @@
+function SaveAsDashboardExtension(dashboardControl) {
+    var _this = this;
+    this.name = "save-as";
+    this.dashboardControl = dashboardControl;
+    this.toolbox = this.dashboardControl.findExtension("toolbox");
+    this.template = "dashboard-save-as-extension"
+
+    this._menuSaveAsItem = new DevExpress.Dashboard.DashboardMenuItem("save-as", "Save As...", 120, 0, function() { _this.visible(true); });
+    this._menuSaveAsItem.hasSeparator = true;
+    this._menuSaveAsItem.data = _this;
+    this.visible = ko.observable(false);
+    this.newName = ko.observable("New Dashboard Name");
+
+    this.saveAs = function () {
+        var newExtension = dashboardControl.findExtension("create-dashboard");
+        newExtension.performCreateDashboard(_this.newName(), dashboardControl.dashboard().getJSON());
+    };
+
+    
+}
+SaveAsDashboardExtension.prototype.start = function () {
+    this.toolbox.menuItems.push(this._menuSaveAsItem);
+    this.toolbox.menuItems().filter(function (item) { return item.id === "save" })[0].hasSeparator = false;
+};
+SaveAsDashboardExtension.prototype.stop = function () {
+    this.toolbox.menuItems.remove(this._menuSaveAsItem);
+    this.toolbox.menuItems().filter(function (item) { return item.id === "save" })[0].hasSeparator = true;
+};
