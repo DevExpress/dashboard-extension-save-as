02bb7281c9fd5ffd84963d811277c6d5fbb8cd65
Andrey Kuleshov <andrey.kuleshov@devexpress.com>
2017-06-27 12:07:52 +0300

    Add namespace

--- save-as-extension.js
+++ save-as-extension.js
@@ -1,31 +1,36 @@
-function SaveAsDashboardExtension(dashboardControl) {
-    var _this = this;
-    this.name = "save-as";
-    this.dashboardControl = dashboardControl;
-    this.toolbox = this.dashboardControl.findExtension("toolbox");
-    this.template = "dashboard-save-as-extension"
+var CustomExtensions;
+(function (CustomExtensions) {
+    function SaveAsDashboardExtension(dashboardControl) {
+        var _this = this;
+        this.name = "save-as";
+        this.dashboardControl = dashboardControl;
+        this.toolbox = this.dashboardControl.findExtension("toolbox");
+        this.template = "dashboard-save-as-extension"
 
-    this._menuSaveAsItem = new DevExpress.Dashboard.DashboardMenuItem("save-as", "Save As...", 120, 0, function() { _this.visible(true); });
-    this._menuSaveAsItem.hasSeparator = true;
-    this._menuSaveAsItem.data = _this;
-    this.visible = ko.observable(false);
-    this.newName = ko.observable("New Dashboard Name");
+        this._menuSaveAsItem = new DevExpress.Dashboard.DashboardMenuItem("save-as", "Save As...", 120, 0, function() { _this.visible(true); });
+        this._menuSaveAsItem.hasSeparator = true;
+        this._menuSaveAsItem.data = _this;
+        this.visible = ko.observable(false);
+        this.newName = ko.observable("New Dashboard Name");
 
-    this.saveAs = function () {
-        var newExtension = dashboardControl.findExtension("create-dashboard");
-        newExtension.performCreateDashboard(_this.newName(), dashboardControl.dashboard().getJSON());
-        _this.hidePopup();
-    };
-    this.hidePopup = function () {
-        _this.visible(false);
+        this.saveAs = function () {
+            var newExtension = dashboardControl.findExtension("create-dashboard");
+            newExtension.performCreateDashboard(_this.newName(), dashboardControl.dashboard().getJSON());
+            _this.hidePopup();
+        };
+        this.hidePopup = function () {
+            _this.visible(false);
+        }
+        
     }
-    
-}
-SaveAsDashboardExtension.prototype.start = function () {
-    this.toolbox.menuItems.push(this._menuSaveAsItem);
-    this.toolbox.menuItems().filter(function (item) { return item.id === "save" })[0].hasSeparator = false;
-};
-SaveAsDashboardExtension.prototype.stop = function () {
-    this.toolbox.menuItems.remove(this._menuSaveAsItem);
-    this.toolbox.menuItems().filter(function (item) { return item.id === "save" })[0].hasSeparator = true;
-};
+    SaveAsDashboardExtension.prototype.start = function () {
+        this.toolbox.menuItems.push(this._menuSaveAsItem);
+        this.toolbox.menuItems().filter(function (item) { return item.id === "save" })[0].hasSeparator = false;
+    };
+    SaveAsDashboardExtension.prototype.stop = function () {
+        this.toolbox.menuItems.remove(this._menuSaveAsItem);
+        this.toolbox.menuItems().filter(function (item) { return item.id === "save" })[0].hasSeparator = true;
+    };
+
+    CustomExtensions.SaveAsDashboardExtension = SaveAsDashboardExtension;
+})(CustomExtensions || (CustomExtensions = {}));
